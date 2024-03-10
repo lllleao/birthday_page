4b8adc7188bbfa54c8f560d6ed8f0ed71bd7d53d
leao <lucasleaolima@gmail.com>
2024-03-10 16:53:24 -0300

    primeiro commit

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init();
+
+    const dataDoEvento = new Date('Mar 19, 2024 06:00:00')
+    const timeStampDoEvento = dataDoEvento.getTime()
+
+    const ateEvento = setInterval(() => {
+        const agora = new Date()
+        const timeStampAgora = agora.getTime()
+
+        const distanciaEmTimeStamp = timeStampDoEvento - timeStampAgora
+
+        const diasAteEvento = Math.floor(distanciaEmTimeStamp / (1000 * 60 * 60 * 24))
+        const horasAteEvento = Math.floor((distanciaEmTimeStamp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const minutosAteEvento = Math.floor((distanciaEmTimeStamp % (1000 * 60 * 60)) / (1000 * 60))
+        const segundosAteEvento = Math.floor((distanciaEmTimeStamp % (1000 * 60)) / (1000))
+
+        document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+
+        if (distanciaEmTimeStamp < 0) {
+            clearInterval(ateEvento)
+            document.getElementById('contador').innerHTML = 'Evento expirado'
+        }
+
+    }, 1000)
+})
